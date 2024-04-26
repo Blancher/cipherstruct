7870b85fa622f65a9e9a0f5e1e1a2f154e324b34
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2024-04-25 17:51:20 -0700

    Update UserCiphers.js

--- frontend/src/pages/UserCiphers.js
+++ frontend/src/pages/UserCiphers.js
@@ -11,28 +11,31 @@ export default function UserCiphers() {
     const [isLoading, error, sendRequest] = useHttp();
     const [ciphers, setCiphers] = useState([]);
     const [reload, setReload] = useState(false);
+    const [empty, setEmpty] = useState(false);
 
     const handleReload = () => setReload(prev => !prev);
 
     useEffect(() => {
         const dataFetcher = async() => {
+            setEmpty(false);
+
             try {
                 const response = await sendRequest('cipher');
-                setCiphers(response.ciphers.filter(cipher => cipher.creator.id === ctx.userId));
+                response.ciphers.length === 0 ? setEmpty(true) : setCiphers(response.ciphers.filter(cipher => cipher.creator.id === ctx.userId));
             } catch(err) {
                 return;
             }
         };
 
         dataFetcher();
-    }, [reload]);
+    }, [reload, ctx]);
 
     return (
         <div className='animate'>
             <h1 className='page'>Your Ciphers</h1>
             {isLoading && <Loading/>}
             {error && <p className='error'>{error}</p>}
-            {!isLoading && !error && ciphers.length === 0 && <h2 className='center displaycard'>You have no ciphers.</h2>}
+            {empty && <h2 className='center displaycard'>You have no ciphers.</h2>}
             {ciphers.length > 0 && (
                 <div className='ciphers flex'>
                     {ciphers.map(cipher => <CipherCard use onReload={handleReload} onDelete={() => {}} {...cipher}/>)}
